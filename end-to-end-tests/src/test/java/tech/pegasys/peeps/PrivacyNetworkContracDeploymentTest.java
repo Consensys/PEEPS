0ed227e0a82e4bc4c24e0e99b2c0248104db2e60
Christopher Hare <chris.hare@consensys.net>
2020-01-02 14:24:09 +1000

    Awating fork dominance

--- end-to-end-tests/src/test/java/tech/pegasys/peeps/PrivacyNetworkContracDeploymentTest.java
+++ end-to-end-tests/src/test/java/tech/pegasys/peeps/PrivacyNetworkContracDeploymentTest.java
@@ -19,6 +19,7 @@ import tech.pegasys.peeps.contract.SimpleStorage;
 import tech.pegasys.peeps.node.model.PrivacyTransactionReceipt;
 import tech.pegasys.peeps.node.model.Transaction;
 import tech.pegasys.peeps.node.model.TransactionReceipt;
+import tech.pegasys.peeps.util.Await;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
@@ -62,13 +63,9 @@ public class PrivacyNetworkContracDeploymentTest {
                 SimpleStorage.BINARY, network.getOrionA(), network.getOrionB());
 
     // Valid transaction receipt for the privacy contract deployment
+    awaitPmtAlignment(receiptHash);
     final TransactionReceipt pmtReceiptNodeA =
         network.getNodeA().getTransactionReceipt(receiptHash);
-    final TransactionReceipt pmtReceiptNodeB =
-        network.getNodeB().getTransactionReceipt(receiptHash);
-
-    assertThat(pmtReceiptNodeA.isSuccess()).isTrue();
-    assertThat(pmtReceiptNodeA).usingRecursiveComparison().isEqualTo(pmtReceiptNodeB);
 
     // Valid privacy marker transaction
     final String hash = pmtReceiptNodeA.getTransactionHash();
@@ -99,5 +96,24 @@ public class PrivacyNetworkContracDeploymentTest {
 
     assertThat(payloadOrionA).isNotNull();
     assertThat(payloadOrionA).isEqualTo(payloadOrionB);
+  }
+
+  // TODO move this method somewhere
+  private void awaitPmtAlignment(final String receiptHash) {
+
+    Await.await(
+        () -> {
+          final TransactionReceipt pmtReceiptNodeA =
+              network.getNodeA().getTransactionReceipt(receiptHash);
+          final TransactionReceipt pmtReceiptNodeB =
+              network.getNodeB().getTransactionReceipt(receiptHash);
+
+          assertThat(pmtReceiptNodeA.isSuccess()).isTrue();
+          assertThat(pmtReceiptNodeA).usingRecursiveComparison().isEqualTo(pmtReceiptNodeB);
+        },
+        "conditionTimeoutMessage");
+
+    // TODO think of a sensible message
+
   }
 }
