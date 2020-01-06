1288f0c467d6736cefd13eb154306e2d63f74b0c
Christopher Hare <chris.hare@consensys.net>
2020-01-06 11:19:24 +1000

    EthHash test outline

---
+++ end-to-end-tests/src/test/java/tech/pegasys/peeps/EthHashConsensusTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.peeps;
+
+import tech.pegasys.peeps.network.Network;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class EthHashConsensusTest {
+
+  @TempDir Path configurationDirectory;
+
+  private Network network;
+
+  @BeforeEach
+  public void setUp() {
+    Runtime.getRuntime().addShutdownHook(new Thread(this::tearDown));
+    network = new Network(configurationDirectory);
+    network.start();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    network.close();
+  }
+
+  @Test
+  public void consensusAfterMiningMustHappen() {
+
+    // TODO no in-line comments - implement clean code!
+
+    // Network Two Besus, no EthSigners or Orions
+
+    // Choose EthHash as consensus mechanism
+
+    // Mine: transfer
+
+    // After suitable time / number of blocks both nodes but agree on state change &
+    // have identical receipt (block number)
+
+  }
+}
