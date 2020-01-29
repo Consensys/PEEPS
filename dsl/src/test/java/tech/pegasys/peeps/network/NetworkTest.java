9817b26e2243b70e924f15d6f3c58f65e6a345bc
Christopher Hare <chris.hare@consensys.net>
2020-01-29 10:03:00 +1000

    Disabling NetworkTest

--- dsl/src/test/java/tech/pegasys/peeps/network/NetworkTest.java
+++ dsl/src/test/java/tech/pegasys/peeps/network/NetworkTest.java
@@ -26,12 +26,14 @@ import java.nio.file.Path;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@Disabled
 @ExtendWith(MockitoExtension.class)
 public class NetworkTest {
 
